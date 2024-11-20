8b68134056dd0330e1e1eaee2329e6f27879e6e5
ktodorow <kristiyantodorow552@gmail.com>
2024-11-20 22:33:42 +0200

    Add Fix for Razor Pages

--- Program.cs
+++ Program.cs
@@ -12,13 +12,15 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+builder.Services.AddRazorPages();
 builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
 {
     options.SignIn.RequireConfirmedAccount = false; // Disable email confirmation for accounts
     options.User.RequireUniqueEmail = false;       // Disable email uniqueness if email isn't used
 })
     .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders();
+    .AddDefaultTokenProviders()
+    .AddDefaultUI();
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
