4ade3bc5843f8042ddd6ae69f0e5fc4c4c120d1d
ktodorov <kristiantodorov552@abv.bg>
2024-11-23 21:19:00 +0200

    Create Generic Repository Interface

---
+++ BeatStore_SoftUni.Data/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace BeatStore_SoftUni.Data.Repository.Interfaces
+{
+    // Generic Repository
+    public interface IRepository<TType, TId>
+    {
+        TType GetById(TId id);
+        Task<TType> GetByIdAsync(TId id);
+        IEnumerable<TType> GetAll();
+        Task<IEnumerable<TType>> GetAllAsync();
+
+        void Add(TType item);
+        Task AddAsync(TType item);
+        bool Delete(TId id);
+        Task<bool> DeleteAsync(TId id);
+        bool SoftDelete( TId id);
+        Task<bool> SoftDeleteAsync(TId id);
+        bool Update(TType item);
+        Task<bool> UpdateAsync(TType item);
+    }
+}
